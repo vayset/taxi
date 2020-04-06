37181228f9e474105b5f089e8f2f95ae77eb315a
Saddam <saddamwolf@gmail.com>
2020-04-06 22:14:49 +0200

    Initial Commit

---
+++ taxi/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  taxi
+//
+//  Created by Saddam Satouyev on 06/04/2020.
+//  Copyright © 2020 Saddam Satouyev. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
